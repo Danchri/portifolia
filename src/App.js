b261b002ffaa8a9137be91ce6aa45827244da83b
Danchri <dantech1438@gmail.com>
2022-11-08 04:33:38 -0800

    'add-component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home"
+import Contact from "./pages/Contact"
+import About from "./pages/About"
+import Skills from "./pages/Skills"
+import Project from "./pages/Project"
 
 function App() {
   return (
-    <div className="App">
-
-      my nAME IS VDANIEL
-      GHGYGYGYGY
-      UYYUUYYU
-       HAHAHA
+    <>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/skills' element={<Skills/>} />
+      <Route path='/projects' element={<Project/>} />
+      <Route path='/contacts' element={<Contact/>} />
+    </Routes>
      
-    </div>
+    </>
   );
 }
 
